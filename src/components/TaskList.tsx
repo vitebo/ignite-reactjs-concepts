b95b1aca2b119f5b3c0dd61bd0cfd37aebe51a2e
André Alves Vitebo <vitebo@hotmail.com>
2021-04-06 14:29:16 -0300

    implements the handleCreateNewTask function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,14 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
